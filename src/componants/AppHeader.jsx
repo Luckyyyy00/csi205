28938ec5f118bfbe7ead0a71db8668e2d75af969
Luckyyyy00 <pubadin.rue@spumail.com>
2025-11-13 23:42:17 +0700

    delete

--- src/componants/AppHeader.jsx
+++
@@ -1,7 +0,0 @@
-const AppHeader = () => {
-    return ( <div  className="d-flex justify-content-center align-items-center text-center bg-primary text-white py-5" style={{backgroundImage:"url('./img/dog.jpg')"}}>
-        <h1 className="fs-2 fw-bold">CSI205-L การพัฒนาโปรแกรมส่วนหน้า</h1>
-    </div> );
-}
- 
-export default AppHeader;
